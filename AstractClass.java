560ec71be58ecf4593675520c57ae1ebc7826f12
许叶杨 <xuyeyang@outlook.com>
2019-10-12 22:51:51 +0800

    Updated to Week04

---
+++ AstractClass.java
@@ -0,0 +1,16 @@
+/*================================================================
+*   Copyright (C) 2019 Navin Xu. All rights reserved.
+*   
+*   Filename    ：AstractClass.java
+*   Author      ：Navin Xu
+*   E-Mail      ：admin@navinxu.com
+*   Create Date ：2019年10月12日
+*   Description ：
+================================================================*/
+
+abstract class AstractClass {
+
+    public int returnANum() {
+        return 123;
+    }
+}
---
+++ FinalField.java
@@ -0,0 +1,24 @@
+/*================================================================
+*   Copyright (C) 2019 Navin Xu. All rights reserved.
+*   
+*   Filename    ：FinalField.java
+*   Author      ：Navin Xu
+*   E-Mail      ：admin@navinxu.com
+*   Create Date ：2019年10月12日
+*   Description ：
+================================================================*/
+public class FinalField {
+    //static final int TESTNUM;
+    final int TESTNUM;
+    public static void main(String[] args) {
+        
+    }
+
+    FinalField() {
+        this.TESTNUM = 30;
+    }
+
+    FinalField(int $i) {
+        this.TESTNUM = 30;
+    }
+}
